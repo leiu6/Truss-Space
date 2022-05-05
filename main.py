3b16e38172077c1dc61bb8f34210a196e4f079b0
Leiu6 <Leighton_Mitchell@outlook.com>
2022-05-05 14:02:10 -0500

    Implemented functions to generate global stiffness matrix for the structure.

--- main.py
+++ main.py
@@ -6,7 +6,22 @@ import solver as s
 
 
 def main():
-    print(s.line_elem_stiffness((1, 1), (2, 2), 20, 1))
+    n1 = (0, 0)
+    n2 = (40, 0)
+    n3 = (40, 30)
+    n4 = (0, 30)
+
+    E = 29.5 * 10 ** 6
+    A = 1
+
+    k1 = s.line_elem_stiffness(n1, n2, E, A)
+    k2 = s.line_elem_stiffness(n3, n2, E, A)
+    k3 = s.line_elem_stiffness(n1, n3, E, A)
+    k4 = s.line_elem_stiffness(n4, n3, E, A)
+
+    gsm = s.line_elem_global_stiffness_matrix([(1, 2), (3, 2), (1, 3), (4, 3)], [k1, k2, k3, k4])
+
+    print(gsm)
 
 
 if __name__ == "__main__":
--- solver.py
+++ solver.py
@@ -1,7 +1,9 @@
 # Truss Space
 # 2D FEA Solver for Trusses
 #
-# All functions for solving matrices
+# All functions for solving trusses
+#
+# Helpful guide: http://www.unm.edu/~bgreen/ME360/Finite%20Element%20Truss.pdf
 
 
 import math
@@ -20,10 +22,10 @@ def line_elem_stiffness(start_point, end_point, E, A):
     y1 = start_point[1]
 
     x2 = end_point[0]
-    y2 = end_point[0]
+    y2 = end_point[1]
 
     L = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)  # line element length
-    c = (x2 - y1) / L  # cosine
+    c = (x2 - x1) / L  # cosine
     s = (y2 - y1) / L  # sine
 
     # generate Numpy array for the stiffness matrix
@@ -33,3 +35,61 @@ def line_elem_stiffness(start_point, end_point, E, A):
         [-c ** 2, -c * s, c * c, c * s],
         [-c * s, -s ** 2, c * s, s * s]
     ])
+
+
+def line_elem_global_stiffness_matrix(nodes, stiffness_matrices):
+    # generates a global stiffness matrix from a list of tuples indicating starting and ending nodes, and a
+    # list of global stiffness matrices for each of those elements.
+    #
+    # nodes: a list of tuples for each element: (start node, end node)
+    # stiffness_matrices: a list of numpy arrays for the element stiffness matrices
+
+    dof = 2*len(nodes)  # Max degrees of freedom. Tells the size of the global stiffness matrix
+
+    # initialize it
+    gsm = np.zeros((dof, dof))
+
+    for i, k in enumerate(stiffness_matrices):
+        # start and end nodes for the element
+        start = nodes[i][0]
+        end = nodes[i][1]
+
+        # global degrees of freedom
+        g_dof = np.array([line_elem_horizontal_dof(start),
+                 line_elem_vertical_dof(start),
+                 line_elem_horizontal_dof(end),
+                 line_elem_vertical_dof(end)])
+
+        # subtract 1 from each so that they correspond to indices
+        g_dof = [x - 1 for x in g_dof]
+
+        # create mini 2x2 matrices
+        print(k)
+        top_left = k[0:2, 0:2]
+        top_right = k[0:2, 2:4]
+        bottom_left = k[2:4, 0:2]
+        bottom_right = k[2:4, 2:4]
+
+        # place them in global matrix
+        gsm[g_dof[0]:g_dof[1] + 1, g_dof[0]:g_dof[1] + 1] += top_left
+        gsm[g_dof[0]:g_dof[1] + 1, g_dof[2]:g_dof[3] + 1] += top_right
+        gsm[g_dof[2]:g_dof[3] + 1, g_dof[0]:g_dof[1] + 1] += bottom_left
+        gsm[g_dof[2]:g_dof[3] + 1, g_dof[2]:g_dof[3] + 1] += bottom_right
+
+    return gsm
+
+
+def line_elem_vertical_dof(node):
+    # computes the vertical degree of freedom for a given node of a line element
+    #
+    # node: the node number of the line element
+
+    return 2 * node
+
+
+def line_elem_horizontal_dof(node):
+    # computes the horizontal degree of freedom for a given node of a line element
+    #
+    # node: the node number of the line element
+
+    return 2 * node - 1
